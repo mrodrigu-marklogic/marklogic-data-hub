76488ed99ada616233fb56744faf380b2772c834
Federico Rufrancos <Federico.rufrancos@marklogic.com>
2021-09-06 12:06:23 -0300

    DHFPROD-7739: Component Replacement: Slider
    replacing slider
    update slider styles

--- marklogic-data-hub-central/ui/src/components/numeric-facet/numeric-facet.tsx
+++ marklogic-data-hub-central/ui/src/components/numeric-facet/numeric-facet.tsx
@@ -1,9 +1,11 @@
 import React, {useState, useEffect, useContext} from "react";
-import {InputNumber, Slider, Tooltip} from "antd";
+import {InputNumber, Tooltip} from "antd";
 import {SearchContext} from "../../util/search-context";
 import {UserContext} from "../../util/user-context";
 import styles from "./numeric-facet.module.scss";
 import {rangeFacet} from "../../api/facets";
+import HCSlider from "../common/hc-slider/hc-slider";
+
 
 interface Props {
   name: any;
@@ -147,7 +149,7 @@ const NumericFacet: React.FC<Props> = (props) => {
     <div className={styles.facetName} >
       <p className={styles.name}>{<Tooltip title={props.name.replace(/\./g, " > ")}>{formatTitle()}</Tooltip>}</p>
       <div className={styles.numericFacet} data-testid="numeric-slider">
-        <Slider className={styles.slider} range={true} value={[range[0], range[1]]} min={rangeLimit[0]} max={rangeLimit[1]} step={props.step} onChange={(e) => onChange(e)} />
+        <HCSlider minLimit={rangeLimit[0]} maxLimit={rangeLimit[1]} min={range[0]} max={range[1]} onChange={(e) => onChange(e)}/>
         <div id={"min-numeric-value"}><InputNumber data-testid="numeric-slider-min" className={styles.inputNumber} value={range[0]} min={rangeLimit[0]} max={rangeLimit[1]} step={props.step} onChange={onChangeMinInput} /></div>
         <div id={"max-numeric-value"}><InputNumber data-testid="numeric-slider-max" className={styles.inputNumber} value={range[1]} min={rangeLimit[0]} max={rangeLimit[1]} step={props.step} onChange={onChangeMaxInput} /></div>
       </div>
